fce042926f3a562ec27098cbd111d4eb6dcd0497
sun-23 <twinsamson@gmail.com>
2019-04-24 13:59:06 +0700

    Initial Commit

---
+++ GetDataOnJson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetDataOnJson
+//
+//  Created by sun on 24/4/2562 BE.
+//  Copyright © 2562 sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
